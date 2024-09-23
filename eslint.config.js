d462ae305f826a1243cbf7ddb23125a6a0183468
darleet <darleet@mail.ru>
2024-09-23 03:17:16 +0300

    app build: linter config fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,18 @@
-const prettier = require("eslint-plugin-prettier");
+const prettier = require('eslint-plugin-prettier');
 
 module.exports = [
   {
-    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
     languageOptions: {
       ecmaVersion: 2021,
-      sourceType: "module",
+      sourceType: 'module',
     },
     plugins: {
       prettier,
     },
     rules: {
-      "prettier/prettier": "error",
-      semi: ["warn", "always"],
+      'prettier/prettier': 'error',
+      semi: ['warn', 'always'],
     },
   },
 ];
