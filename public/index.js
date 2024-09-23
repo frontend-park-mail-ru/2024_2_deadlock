f4145a6adcb0fa7ea248b60364f9a0c1d498f4ad
Alexander Novak <shura.novak.03@mail.ru>
2024-09-24 02:03:44 +0300

    feed fix: fix errors in html code

--- public/index.js
+++ public/index.js
@@ -1,9 +1,13 @@
 import Cards from './components/Cards/cards.js';
 
 const cardsCollection = [
-  { title: 'What is Lorem Ipsum?', description: 'text1', imageUrl: 'image1.jpg' },
+  {
+    title: 'What is Lorem Ipsum?',
+    description: 'text1',
+    imageUrl: 'images/1.jpg',
+  },
   { title: 'title2', description: 'text2' },
-  { title: 'title3', description: 'text3', imageUrl: 'image3.jpg' },
+  { title: 'title3', description: 'text3', imageUrl: 'images/2.jpg' },
 ];
 
 const feed = document.querySelector('.feed');
