53ae4942d0065786e50afb1bf74d9785606be145
Alexander Novak <shura.novak.03@mail.ru>
2024-09-24 13:57:58 +0300

    feed fix: fix errors in cards style

--- public/index.js
+++ public/index.js
@@ -2,12 +2,46 @@ import Cards from './components/Cards/cards.js';
 
 const cardsCollection = [
   {
-    title: 'What is Lorem Ipsum?',
-    description: 'text1',
+    title: 'What is Lorem Ipsum? What is Lorem Ipsum?',
+    description: `text1 text1 text1 text1 text1 text1 text text1 text1 text1 text1 
+      text1 text1 text text1 text1 text1 text1 text1 text1 text text1 text1  
+      text1 text1 text1 text1 text text1 text1 text1 text1 text1 text1 text 
+      text1 text1 text1 text1 text1 text1 text text1 text1 text1 text1 
+      text1 text1 text text1 text1 text1 text1 text1 text1 text text1 text1  
+      text1 text1 text1 text1 text text1 text1 text1 text1 text1 text1 text 
+      `,
     imageUrl: 'images/1.jpg',
   },
-  { title: 'title2', description: 'text2' },
-  { title: 'title3', description: 'text3', imageUrl: 'images/2.jpg' },
+  {
+    title: 'title2',
+    description: `text1 text1 text1 text1 text1 text1 text text1 text1 text1 text1 
+      text1 text1 text text1 text1 text1 text1 text1 text1 text text1 text1  
+      `,
+  },
+  {
+    title: 'title3',
+    description: `text1 text1 text1 text1 text1 text1 text text1 text1 text1 text1 
+      text1 text1 text text1 text1 text1 text1 text1 text1 text text1 text1  
+      text1 text1 text1 text1 text text1 text1 text1 text1 text1 text1 text 
+      text1 text1 text1 text1 text1 text1 text text1 text1 text1 text1 text1 
+      text1 text text1 text1 text1 text1 text1 text1 text`,
+    imageUrl: 'images/2.jpg',
+  },
+  {
+    title: 'title4',
+    description: `text1 text1 text1 text1 text1 text1 text text1 text1 text1 text1 
+      text1 text1 text text1 text1 text1 text1 text1 text1 text text1 text1  
+      `,
+  },
+  {
+    title: 'title3',
+    description: `text1 text1 text1 text1 text1 text1 text text1 text1 text1 text1 
+      text1 text1 text text1 text1 text1 text1 text1 text1 text text1 text1  
+      text1 text1 text1 text1 text text1 text1 text1 text1 text1 text1 text 
+      text1 text1 text1 text1 text1 text1 text text1 text1 text1 text1 text1 
+      text1 text text1 text1 text1 text1 text1 text1 text`,
+    imageUrl: 'images/3.jfif',
+  },
 ];
 
 const feed = document.querySelector('.feed');
