6e78352cf8beddb707360d3b9a192bbad61474b7
Georg <georgarshakyan@yandex.ru>
2024-10-08 02:01:17 +0300

    DEAD-14 feat: Include ts, change user.js to user.ts

--- public/user.js
+++ public/user.js
@@ -1,25 +1,32 @@
-class UserState {
-  constructor() {
+'use strict';
+// interface UserState {
+// }
+// Object.defineProperty(exports, "__esModule", { value: true });
+var UserState = /** @class */ (function () {
+  function UserState() {
     this.isAuthorized = false;
     this.email = '';
+    this.name = '';
   }
-
-  login() {
+  UserState.prototype.login = function () {
     this.isAuthorized = true;
-  }
-
-  logout() {
+  };
+  UserState.prototype.logout = function () {
     this.isAuthorized = false;
-  }
-
-  setEmail(email) {
+  };
+  UserState.prototype.setEmail = function (email) {
     this.email = email;
-  }
-
-  removeEmail() {
+  };
+  UserState.prototype.removeEmail = function () {
     this.email = '';
-  }
-}
-
-const userState = new UserState();
+  };
+  UserState.prototype.setName = function (name) {
+    this.name = name;
+  };
+  UserState.prototype.removeName = function () {
+    this.name = '';
+  };
+  return UserState;
+})();
+var userState = new UserState();
 export default userState;
