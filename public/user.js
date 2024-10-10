ee53fe1e0f544ca0350f68008efdfdbe601132c0
Georg <georgarshakyan@yandex.ru>
2024-10-10 20:07:55 +0300

    DEAD-14 feat: Add user fields, add styles

--- public/user.js
+++ public/user.js
@@ -1,10 +1,16 @@
 'use strict';
+// Object.defineProperty(exports, "__esModule", { value: true });
 var UserState = /** @class */ (function () {
   function UserState() {
     this.isAuthorized = false;
     this.email = '';
     this.name = '';
     this.password = '';
+    this.avatar = '';
+    this.registrationDate = '';
+    this.subcribers = 0;
+    this.subscriptions = 0;
+    this.header = '';
   }
   UserState.prototype.login = function () {
     this.isAuthorized = true;
@@ -30,7 +36,36 @@ var UserState = /** @class */ (function () {
   UserState.prototype.removePassword = function () {
     this.password = '';
   };
+  UserState.prototype.setAvatar = function (path) {
+    this.avatar = path;
+  };
+  UserState.prototype.setHeader = function (path) {
+    this.header = path;
+  };
+  UserState.prototype.removeAvatar = function () {
+    this.avatar = '';
+  };
+  UserState.prototype.setRegistrationDate = function (date) {
+    this.registrationDate = date;
+  };
+  UserState.prototype.removeDate = function () {
+    this.registrationDate = '';
+  };
+  UserState.prototype.setSubscribers = function (subcribers) {
+    this.subcribers = subcribers;
+  };
+  UserState.prototype.setSubscriptions = function (subscriptions) {
+    this.subscriptions = subscriptions;
+  };
   return UserState;
 })();
 var userState = new UserState();
+userState.setName('MyUser');
+userState.setEmail('username@email.com');
+userState.setPassword('MyPassword');
+userState.setRegistrationDate('10.10.2024');
+userState.setAvatar('../../images/leonardo.osnova.png');
+userState.setHeader('../../images/5b6eca63605bea0eeb48db43f77fa0ce.jpg');
+userState.setSubscribers(13);
+userState.setSubscriptions(20);
 export default userState;
--- public/user.ts
+++ public/user.ts
@@ -1,13 +1,26 @@
-class UserState {
+interface UserState {
   isAuthorized: boolean;
   email: string;
   name: string;
   password: string;
+  avatar: string;
+  registrationDate: string;
+  subcribers: number;
+  subscriptions: number;
+  header: string;
+}
+
+class UserState {
   constructor() {
     this.isAuthorized = false;
     this.email = '';
     this.name = '';
     this.password = '';
+    this.avatar = '';
+    this.registrationDate = '';
+    this.subcribers = 0;
+    this.subscriptions = 0;
+    this.header = '';
   }
 
   login() {
@@ -41,7 +54,43 @@ class UserState {
   removePassword() {
     this.password = '';
   }
+
+  setAvatar(path: string) {
+    this.avatar = path;
+  }
+
+  setHeader(path: string) {
+    this.header = path;
+  }
+
+  removeAvatar() {
+    this.avatar = '';
+  }
+
+  setRegistrationDate(date: string) {
+    this.registrationDate = date;
+  }
+
+  removeDate() {
+    this.registrationDate = '';
+  }
+
+  setSubscribers(subcribers: number) {
+    this.subcribers = subcribers;
+  }
+
+  setSubscriptions(subscriptions: number) {
+    this.subscriptions = subscriptions;
+  }
 }
 
 const userState = new UserState();
+userState.setName('MyUser');
+userState.setEmail('username@email.com');
+userState.setPassword('MyPassword');
+userState.setRegistrationDate('10.10.2024');
+userState.setAvatar('../../images/leonardo.osnova.png');
+userState.setHeader('../../images/5b6eca63605bea0eeb48db43f77fa0ce.jpg');
+userState.setSubscribers(13);
+userState.setSubscriptions(20);
 export default userState;
