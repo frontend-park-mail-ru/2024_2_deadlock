ba10d236b76e5a5d2ef0c2a72b3b1619249c5fab
Georg <georgarshakyan@yandex.ru>
2024-10-18 20:26:16 +0300

    DEAD-14 feat: Add username symbolscounter

--- src/components/Settings/settings.js
+++ src/components/Settings/settings.js
@@ -1,20 +1,18 @@
 'use strict';
+
+// import userState from "../../user/user";
+
 // Object.defineProperty(exports, "__esModule", { value: true });
 // var user_js_1 = require("../../user/user.js");
 import userState from '../../user/user.js';
-// var Handlebars = require("handlebars");
-// import Handlebars from "../../../node_modules/handlebars/types/types.d.ts";
+// var handlebars_1 = require("handlebars");
+import Handlebars from 'handlebars';
 var MAIN_POPULAR = 'mainPopular';
 var MAIN_LATEST = 'mainLatest';
 var MAIN_OWN = 'mainOwn';
 var SORT_BY_POPULARITY = 'sortByPopularity';
 var SORT_BY_DATE = 'sortByDate';
 var MAX_NAME_LENGTH = 30;
-
-Handlebars.registerHelper('split', function (str) {
-  return str.split('');
-});
-
 var Settings = /** @class */ (function () {
   function Settings(parent) {
     this.parent = parent;
@@ -27,10 +25,11 @@ var Settings = /** @class */ (function () {
     };
   }
   Settings.prototype.render = function () {
-    var template = Handlebars.templates['settings.hbs'];
-    this.parent.innerHTML = template({ user: userState, context: this.context });
+    var template = Handlebars.default.templates['settings.hbs'];
+    this.parent.innerHTML = template({ user: userState.default, context: this.context });
     var inputCounter = document.querySelector('.input-counter');
     var inputField = document.querySelector('#name-input');
+    inputField.value = userState.default.name.length.toString();
     var inputLength = inputField.value.length;
     console.log(inputField);
     console.log(inputLength);
@@ -48,6 +47,18 @@ var Settings = /** @class */ (function () {
     document.addEventListener('DOMContentLoaded', function (event) {
       countHandler();
     });
+    Handlebars.default.registerHelper('split', function (str) {
+      return str.split('');
+    });
+    var linkConfirm = document.querySelector('.link-confirm[name=name-description-save]');
+    var form = document.querySelector('#name-description-form');
+    console.log(linkConfirm);
+    linkConfirm === null || linkConfirm === void 0
+      ? void 0
+      : linkConfirm.addEventListener('click', function (event) {
+          event.preventDefault();
+          form.submit();
+        });
   };
   return Settings;
 })();
