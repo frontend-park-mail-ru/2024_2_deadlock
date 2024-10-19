9384b26a47710b7ee7f33c51b83849777a52cb8a
Владислав Кирпичов <v.kirpichov@vk.team>
2024-10-19 23:01:13 +0300

    declare

---
+++ src/components/Fields/TextField/textfield.js
@@ -0,0 +1,58 @@
+import { textStyles } from '../fieldconstants.js';
+import template from './textfield.hbs';
+
+export default class TextField {
+  constructor(parent, id, textStyle, prevID, nextID) {
+    this.parent = parent;
+    this.id = id;
+    this.textStyle = textStyle;
+    this.node = undefined;
+    this.makeTextFunc = this.makeText.bind(this);
+    this.makeHeaderFunc = this.makeHeader.bind(this);
+  }
+
+  render() {
+    // const template = Handlebars.templates['textfield.hbs'];
+    const prevElement = this.parent.querySelector(`.field[data-id="${this.prevID}"]`);
+
+    if (prevElement) {
+      prevElement.insertAdjacentHTML('afterend', template({ element: this }));
+    } else {
+      this.parent.insertAdjacentHTML('afterbegin', template({ element: this }));
+    }
+
+    this.node = this.parent.querySelector(`.field[data-id="${this.id}"]`);
+    const text_button = this.node.querySelector('.make-text-href');
+    const header_button = this.node.querySelector('.make-header-href');
+
+    header_button.addEventListener('click', this.makeHeaderFunc);
+    text_button.addEventListener('click', this.makeTextFunc);
+  }
+
+  makeHeader(event) {
+    console.log(event.target);
+    this.changeTextStyle(this.id, textStyles.HEADER);
+  }
+
+  makeText(event) {
+    console.log(event.target);
+    this.changeTextStyle(this.id, textStyles.REGULAR);
+  }
+
+  changeTextStyle(id, textStyle) {
+    let cur = this.node.querySelector('.div-input');
+    cur.classList.remove(this.textStyle);
+    this.textStyle = textStyle;
+    cur.classList.add(this.textStyle);
+  }
+
+  delete() {
+    const text_button = this.node.querySelector('.make-text-href');
+    const header_button = this.node.querySelector('.make-header-href');
+
+    text_button.removeEventListener('click', this.makeTextFunc);
+    header_button.removeEventListener('click', this.makeHeaderFunc);
+
+    this.node.remove();
+  }
+}
