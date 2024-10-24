e6b9aab93e63bbc4c15e3a86b9ed776879ce3668
Malkhasyan Yu. V. <malkhasyan.yur@yandex.ru>
2024-10-24 20:42:06 +0300

    temp commit

--- src/api/api_profile.ts
+++ src/api/api_profile.ts
@@ -11,7 +11,7 @@ class ProfileApi {
 
   async getProfile(){
     const response = await Ajax({
-      url: `${this.url}${ApiPaths.profile}${name}`,
+      url: `${this.url}${ApiPaths.profile}`,
       method: 'GET',
     });
 
@@ -33,8 +33,7 @@ class ProfileApi {
     }
     return answer;
   }
-
-
-
-  
 }
+
+
+export default new ProfileApi(`${ApiPaths.baseUrl}`);
--- src/components/Profile/profile.ts
+++ src/components/Profile/profile.ts
@@ -2,9 +2,11 @@ import Handlebars from 'handlebars';
 import userState from '../../user/user';
 import templates from './profile.hbs';
 import styles from './profile.css';
+import ProfileApi from '../../api/api_profile';
 
 class Profile {
   parent: Element;
+  header: {};
   context: {
     isPosts: boolean;
   };
@@ -13,11 +15,22 @@ class Profile {
     this.context = {
       isPosts: true,
     };
+    this.header = '';
   }
 
-  render() {
-    this.parent.innerHTML = templates({ user: userState, context: this.context });
+  async render() {
+    this.header = await ProfileApi.getProfile();
+    this.parent.innerHTML = templates({ 
+      user: userState, 
+      context: this.context, 
+      header: this.header 
+    });
     // var disabledHref = document.querySelector('.disabled-href');
   }
+
+  setHeader(url: string) {
+    this.header = url;
+  }
 }
+
 export default Profile;
